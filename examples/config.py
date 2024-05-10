1651ee1b7f0231ae5c91f1dd5f693ce156ac9156
Lunarmagpie <bambolambo0@gmail.com>
2024-05-10 17:31:19 -0400

    remove dist

--- examples/config.py
+++ examples/config.py
@@ -2,5 +2,5 @@ from desktop_thingies import Texture
 
 objects = [
     Texture(texture="examples/reimu_fumo.png", scale=1 / 6),
-    Texture(texture="examples/reimu_fumo.png", scale=1 / 5),
+    Texture(texture="examples/reimu_fumo.png", scale=1 / 5, displays=["DP-3"]),  # Display this fumo only on DP-3
 ]
